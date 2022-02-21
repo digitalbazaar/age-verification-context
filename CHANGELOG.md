a4cbe2ddd783da896e109ccfd2f01ab4baef42b6
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-02-21 13:01:47 -0500

    Initial commit.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# context-template ChangeLog
+# @digitalbazaar/age-verification-context ChangeLog
 
 ## 1.0.0 - TBD
 
--- js/context.js
+++ js/context.js
@@ -5,9 +5,62 @@
 
 module.exports = {
   '@context': {
+    '@protected': 'true',
     id: '@id',
     type: '@type',
-    '@protected': true
 
+    // Used for several VCs (for issuer display)
+    description: 'https://schema.org/description',
+    image: {
+      '@id': 'https://schema.org/image',
+      '@type': '@id'
+    },
+    name: 'https://schema.org/name',
+
+    // Used in OverAgeTokenCredential and AgeVerificationCredential
+    overAge: {
+      '@id': 'https://w3id.org/age#overAge',
+      '@type': 'http://www.w3.org/2001/XMLSchema#positiveInteger'
+    },
+
+    concealedIdToken: {
+      '@id': 'https://w3id.org/cit#concealedIdToken',
+      '@type': 'https://w3id.org/security#multibase'
+    },
+    anchoredResource: {
+      '@type': '@id',
+      '@id': 'https://w3id.org/security#anchoredResource'
+    },
+    contentHash: {
+      '@id': 'https://w3id.org/security#contentHash',
+      '@type': 'https://w3id.org/security#multibase'
+    },
+
+    PersonalPhotoCredential:
+      'https://convenience.org/vocab#PersonalPhotoCredential',
+
+    OverAgeTokenCredential: 'https://w3id.org/age#OverAgeTokenCredential',
+
+    // refreshService section of the VC
+    VerifiableCredentialRefreshService2021: {
+      // eslint-disable-next-line max-len
+      '@id': 'https://w3id.org/vc-refresh-service#VerifiableCredentialRefreshService2021',
+      '@context': {
+        '@protected': true,
+        url: {
+          '@id': 'https://schema.org/url',
+          '@type': '@id'
+        },
+        refreshToken: {
+          '@id': 'https://w3id.org/vc-refresh-service#refreshToken',
+          '@type': 'https://w3id.org/security#multibase'
+        }
+      }
+    },
+
+    AgeVerificationCredential: 'https://w3id.org/age#AgeVerificationCredential',
+
+    AgeVerificationContainerCredential:
+      'https://w3id.org/age#AgeVerificationContainerCredential'
   }
 };
