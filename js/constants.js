8e7f569b3bf400ebabc2143f448538860131f968
David I. Lehn <dlehn@digitalbazaar.com>
2024-06-19 00:21:30 -0400

    Add CBORLD_CODEC_VALUE constant.

--- js/constants.js
+++ js/constants.js
@@ -1,9 +1,10 @@
 /*!
- * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2021-2024 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
 module.exports = {
   CONTEXT_FILENAME: 'age-v1.jsonld',
-  CONTEXT_URL_V1: 'https://w3id.org/age/v1'
+  CONTEXT_URL_V1: 'https://w3id.org/age/v1',
+  CBORLD_CODEC_VALUE: 0x16
 };
